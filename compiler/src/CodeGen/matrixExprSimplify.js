f40409ce108bd420e9b06db346285961111fff92
nandu6177 <v.nandagopal6@gmail.com>
2018-06-26 15:59:27 +0530

    upd

--- compiler/src/CodeGen/matrixExprSimplify.js
+++ compiler/src/CodeGen/matrixExprSimplify.js
@@ -9,38 +9,51 @@ function transform(ast,ctx){
 function fdefs(ast,ctx){
 	for(var i = 0; i < ast.length ; i++){
 		ctx.symtbl.enterNestedScope(ast[i].id);
+		// console.log(ast[i].body.stmts);
 		stmts(ast[i].body.stmts,ctx);
 		ctx.symtbl.exitNestedScope();
 	}
 }
 
-function block(obj,ctx){
-	if(typeof obj.stmts != 'undefined'){
-		for(var i in obj.stmts){
-			stmts(obj.stmts[i]);
+function block(ast,ctx){
+	if(typeof ast.stmts != 'undefined'){
+		for(var i in ast.stmts){
+			stmts(ast.stmts[i],ctx);
 		}
 	}
 }
 
-function stmts(obj, ctx){
-	if(typeof obj.kind != 'undefined'){
-		switch(obj.kind){
-			case "assign":
-				expr(obj.expr, ctx, true);
-				break;
-			case "block":
-				block(obj,str);
-				break;
+function stmts(ast, ctx){
+	for(var i=0;i<ast.length;i++){
+		if(typeof ast[i].kind != 'undefined'){
+			switch(ast[i].kind){
+				case "assign":
+					var Separate=expr(ast[i].expr, ctx, true);
+					// console.log(Separate);
+					break;
+				case "block":
+					block(ast[i],ctx);
+					break;
+			}
 		}
 	}
 }
 
-function expr(obj, ctx, isRoot){
-	if(typeof obj.op != 'undefined'){
-		if(typeof obj.lexpr.op != 'undeifned' || typeof obj.rexpr.op !='undefined'){
-
-		}
+function expr(ast, ctx, isRoot){
+	if(typeof ast.op != 'undefined'){
+		if((expr(ast.lexpr,ctx,false) || expr(ast.rexpr,ctx,false)))
+			return true;
 	}
+	else if(typeof ast.id != 'undefined'){
+		var dim=0;
+		if(typeof ast.dim != 'undefined'){
+			dim=ast.dim.dim.length;
+			console.log((ctx.symtbl.lookup(ast.id).info.type.dim.dim.length > dim));
+		}
+		if((ctx.symtbl.lookup(ast.id).info.type.dim.dim.length > dim) && !isRoot)
+			return true;
+	}
+	return false;
 }
 
 var temp_ind=0;
