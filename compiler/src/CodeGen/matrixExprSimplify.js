51d7eda9098436154d7825d3413689e41f291ff9
nandu6177 <v.nandagopal6@gmail.com>
2018-06-26 12:47:24 +0530

    nandu upd

--- compiler/src/CodeGen/matrixExprSimplify.js
+++ compiler/src/CodeGen/matrixExprSimplify.js
@@ -1,14 +1,16 @@
-module.exports = function(ast,ctx){
+function transform(ast,ctx){
 	for(var i in ast.modules){
-		// ctx.symtbl.enterNestedScope(i);
-		// module(ast.module[i],ctx);
-		// ctx.symtbl.exitNestedScope();
-		console.log(i);
+		ctx.symtbl.enterNestedScope(i);
+		module(ast.modules[i],ctx);
+		ctx.symtbl.exitNestedScope();
+		// console.log(i);
 	}
 };
 
 function module(ast,ctx){
-	// ctx.symtbl.enterNestedScope(lol);
-	// console.log(ctx.symtbl.lookup(a));
+	ctx.symtbl.enterNestedScope("lol");
+	console.log(ctx.symtbl.lookup("a"));
 
 }
+
+exports.transform=transform;
