2237348ee8ea7fe0dff17b1bfacf6b0a46c788af
nandu6177 <v.nandagopal6@gmail.com>
2018-06-27 12:24:59 +0530

    upd

--- compiler/src/CodeGen/matrixExprSimplify.js
+++ compiler/src/CodeGen/matrixExprSimplify.js
@@ -9,8 +9,8 @@ function transform(ast,ctx){
 function fdefs(ast,ctx){
 	for(var i = 0; i < ast.length ; i++){
 		ctx.symtbl.enterNestedScope(ast[i].id);
-		// console.log(ast[i].body.stmts);
 		stmts(ast[i].body.stmts,ctx);
+		// console.log(JSON.stringify(ast[i].body.stmts));
 		ctx.symtbl.exitNestedScope();
 	}
 }
@@ -28,7 +28,13 @@ function stmts(ast, ctx){
 		if(typeof ast[i].kind != 'undefined'){
 			switch(ast[i].kind){
 				case "assign":
-					assign(ast[i], ctx);
+					var orig=assign(ast[i], ctx);
+					// console.log(block_stmts.length);
+					if(block_stmts.length>0){
+						ast[i]={kind: "block", stmts: block_stmts, orig: orig};
+						block_stmts=[];
+					}
+					// console.log(JSON.stringify(ast[i]));
 					break;
 				case "block":
 					block(ast[i],ctx);
@@ -40,10 +46,21 @@ function stmts(ast, ctx){
 
 function assign(ast, ctx){
 	var orig = astlib.deep_copy(ast);
-	expr(ast.expr, ctx, true);
+	var expre=expr(ast.expr, ctx, true);
+	// console.log(JSON.stringify(ast));
+	// console.log("-----------------------------");
+	// console.log(JSON.stringify(expre));
 	if(block_stmts.length > 0){
-
+		if(typeof ast.qid != 'undefined'){
+			block_stmts.push({kind: "assign", id: ast.qid[0], expr: expre})
+		}
+		else if(typeof ast.id != 'undefined'){
+			var assign_stmt = JSON.parse(JSON.stringify(ast));
+			assign_stmt.expr=expre;
+			block_stmts.push(assign_stmt);
+		}
 	}
+	return orig;
 }
 
 function is_varconst(ast){
@@ -60,6 +77,7 @@ function is_dim_resolved(ast,ctx){
 	if(typeof ast.dim != 'undefined'){
 		dim=ast.dim.dim.length;
 	}
+	// console.log(ast.id);
 	if(typeof ctx.symtbl.lookup(ast.id).info.type.dim != 'undefined'){
 		if(ctx.symtbl.lookup(ast.id).info.type.dim.dim.length > dim)
 			return false;
@@ -68,31 +86,35 @@ function is_dim_resolved(ast,ctx){
 }
 
 function get_dim(ast,ctx){
-	if(ast.expr.op == "+"){
-		if(typeof ast.expr.lexpr.id != 'undefined'){
-				return ctx.symtbl.lookup(ast.expr.lexpr.id).info.type.dim;
+	// console.log(ast);
+	if(ast.op == "+"){
+		if(typeof ast.lexpr.id != 'undefined'){
+				return {dim : ctx.symtbl.lookup(ast.lexpr.id).info.type.dim, info:ctx.symtbl.lookup(ast.lexpr.id).info};
 		}
 		else
-			return ctx.symtbl.lookup(ast.expr.lexpr.qid[0]).info.type.dim;
+			return {dim:ctx.symtbl.lookup(ast.lexpr.qid[0]).info.type.dim , info: ctx.symtbl.lookup(ast.lexpr.qid[0]).info};
 	}
-	else if(ast.expr.op == "*"){
-		if(typeof ast.expr.lexpr.id != 'undefined'){
-				return { dim : [ctx.symtbl.lookup(ast.expr.lexpr.id).info.type.dim.dim[0],ctx.symtbl.lookup(ast.expr.rexpr.id).info.type.dim.dim[1]], info : ctx.symtbl.lookup(ast.expr.lexpr.id).info};
+	else if(ast.op == "*"){
+		if(typeof ast.lexpr.id != 'undefined'){
+				return { dim : [ctx.symtbl.lookup(ast.lexpr.id).info.type.dim.dim[0],ctx.symtbl.lookup(ast.rexpr.id).info.type.dim.dim[1]], info : ctx.symtbl.lookup(ast.lexpr.id).info};
 		}
 		else
-			return {dim : [ctx.symtbl.lookup(ast.expr.lexpr.qid[0]).info.type.dim[0],ctx.symtbl.lookup(ast.expr.rexpr.qid[0]).info.type.dim[1]], info: ctx.symtbl.lookup(ast.expr.lexpr.qid[0]).info};	
+			return {dim : [ctx.symtbl.lookup(ast.lexpr.qid[0]).info.type.dim[0],ctx.symtbl.lookup(ast.rexpr.qid[0]).info.type.dim[1]], info: ctx.symtbl.lookup(ast.lexpr.qid[0]).info};	
 	}
 }
 
 function transform_expr(ast, ctx){
-	var details = get_dim(ast.expr, ctx);
+	var details = get_dim(ast, ctx);
+	// console.log(details);
 	details.info.type.dim.dim = details.dim;
-	block_stmts.push({kind : "assign", id : "$t"+temp_ind, expr : ast.expr});
-	ctx.symtbl.addSymbolToCurrentScope("$t"+temp_ind , info);
+	// console.log(JSON.stringify(ast));
+	block_stmts.push({kind: "assign",id : "$t"+temp_ind,expr: JSON.parse(JSON.stringify(ast))});
+	ctx.symtbl.addSymbolToCurrentScope("$t"+temp_ind , details.info);
 	return {id : "$t"+temp_ind++};
 }
 
 function expr(ast, ctx, isRoot){
+	// console.log(JSON.stringify(ast));
 	if(typeof ast.op != 'undefined'){
 		var lexpr_is_varconst = is_varconst(ast.lexpr);
 		var rexpr_is_varconst = is_varconst(ast.rexpr);
@@ -106,17 +128,21 @@ function expr(ast, ctx, isRoot){
 	}
 	else{
 		if(!lexpr_is_varconst){
-			var texpr = expr(ast.lexpr, false);
+			var texpr = expr(ast.lexpr, ctx, false);
 			if(texpr)
 				ast.lexpr = texpr;
 		}
 		if(!rexpr_is_varconst){
-			var texpr = expr(ast.rexpr, false);
+			var texpr = expr(ast.rexpr, ctx, false);
 			if(texpr){
 				ast.rexpr = texpr;
 			}
 		}
 	}
+	return ast;
+	console.log("----------------------------------");
+	block_stmts.push({kind : "assign", id : " ", expr : ast});
+	console.log(JSON.stringify(block_stmts[1]));
 
 }
 
