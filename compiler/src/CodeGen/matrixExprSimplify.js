02f6c3830e0b74404071063a3a236de4bc3a349c
nandu6177 <v.nandagopal6@gmail.com>
2018-06-27 16:44:05 +0530

    upd

--- compiler/src/CodeGen/matrixExprSimplify.js
+++ compiler/src/CodeGen/matrixExprSimplify.js
@@ -4,7 +4,7 @@ function transform(ast,ctx){
 		fdefs(ast.modules[i].fdefs,ctx);
 		ctx.symtbl.exitNestedScope();
 	}
-};
+}
 
 function fdefs(ast,ctx){
 	for(var i = 0; i < ast.length ; i++){
@@ -86,20 +86,37 @@ function is_dim_resolved(ast,ctx){
 }
 
 function get_dim(ast,ctx){
-	// console.log(ast);
-	if(ast.op == "+"){
+	var Left;
+	var Right;
+	if(ast.op == "+" || ast.op == "-"){
 		if(typeof ast.lexpr.id != 'undefined'){
-				return {dim : ctx.symtbl.lookup(ast.lexpr.id).info.type.dim, info:ctx.symtbl.lookup(ast.lexpr.id).info};
+			Left = ctx.symtbl.lookup(ast.lexpr.id);
+			return {dim : Left.info.type.dim, info:Left.info};
 		}
-		else
-			return {dim:ctx.symtbl.lookup(ast.lexpr.qid[0]).info.type.dim , info: ctx.symtbl.lookup(ast.lexpr.qid[0]).info};
+		else{
+			Left = ctx.symtbl.lookup(ast.lexpr.qid[0]);
+			return {dim: Left.info.type.dim , info: Left.info};
+		}
 	}
 	else if(ast.op == "*"){
-		if(typeof ast.lexpr.id != 'undefined'){
-				return { dim : [ctx.symtbl.lookup(ast.lexpr.id).info.type.dim.dim[0],ctx.symtbl.lookup(ast.rexpr.id).info.type.dim.dim[1]], info : ctx.symtbl.lookup(ast.lexpr.id).info};
-		}
-		else
-			return {dim : [ctx.symtbl.lookup(ast.lexpr.qid[0]).info.type.dim[0],ctx.symtbl.lookup(ast.rexpr.qid[0]).info.type.dim[1]], info: ctx.symtbl.lookup(ast.lexpr.qid[0]).info};	
+			if(typeof ast.lexpr.id != 'undefined' && typeof ast.rexpr.id != 'undefined'){
+				Left = ctx.symtbl.lookup(ast.lexpr.id);
+				Right = ctx.symtbl.lookup(ast.rexpr.id);
+			}
+			else if(typeof ast.lexpr.id != 'undefined' && typeof ast.rexpr.qid != 'undefined'){
+				Left = ctx.symtbl.lookup(ast.lexpr.id);
+				Right = ctx.symtbl.lookup(ast.rexpr.qid[0]);
+			}
+			else if(typeof ast.lexpr.qid != 'undefined' && typeof ast.rexp.id != 'undefined'){
+				Left = ctx.symtbl.lookup(ast.lexpr.qid[0]);
+				Right = ctx.symtbl.lookup(ast.rexpr.id);
+			}
+			else if(typeof ast.lexpr.qid != 'undefined' && typeof ast.rexpr.qid != 'undefined'){
+				Left = ctx.symtbl.lookup(ast.lexpr.qid[0]);
+				Right = ctx.symtbl.lookup(ast.rexpr.qid[0]);
+			}
+			if(Left.info.type.dim.dim.length === 2 && Right.info.type.dim.dim.length === 2)
+			return { dim : [Left.info.type.dim.dim[0],Right.info.type.dim.dim[1]], info : Left.info};
 	}
 }
 
