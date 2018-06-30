fa1f1f0da0f27794df9f0bbeb795db2778e255a7
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2018-06-30 15:41:48 +0530

    compiler small changes

---
+++ compiler/src/vtparams.js
@@ -0,0 +1,6 @@
+module.exports = [
+	  "-xast", __dirname + "/pseq.js"
+	, "-xast", __dirname + "/duseq.js"
+	, "-xast", __dirname + "/stdalloc.js"
+//	, "-xast", __dirname + "/CodeGen/CodeGen.js"
+];
