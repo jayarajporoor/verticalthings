6905640b260e0caee327efbe112c4466ff410245
Sandesh Ghanta <sghanta05@gmail.com>
2018-07-05 21:48:07 +0530

    Updated analysis to include header files

---
+++ tools/testing/testing/send.py
@@ -0,0 +1,22 @@
+from struct import *
+import serial
+import smbus
+import time
+import os
+file = open('data.csv','r')
+data = []
+for line in file:
+    data.append(line)
+file.close()
+ser = serial.Serial('/dev/ttyACM0',9600)
+for line in data:
+    intvalues = [int(value) for value in line.split(',')]
+    ser.write(pack('>hhhhhh',intvalues[0],intvalues[1],intvalues[2],intvalues[3],intvalues[4],intvalues[5]))
+    time.sleep(0.05)
+    result = b""
+    while (ser.inWaiting() > 0):
+        result += ser.read()
+    print (result)
+#    print (unpack('>hhhhhh',result))
+ser.close()
+
