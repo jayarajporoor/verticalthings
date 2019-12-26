bc89f526d7b0a8844fb34c216cec6b03fe866a3e
Jayaraj Poroor <jayaraj.poroor@paanini.com>
2019-12-26 07:14:16 +0530

    compilation test for generated code passed

--- sample_compile.sh
+++ sample_compile.sh
@@ -1,2 +1,3 @@
 nodejs  compiler/src2/vtcompiler.js apps/dsl2-samples/syntax_test5.vt -config apps/dsl2-samples/compile-config.json -entry syntax_test5 -code syntax_test5.ino
 nodejs  compiler/src2/vtcompiler.js apps/dsl2-samples/syntax_test5.vt -config apps/dsl2-samples/compile-config.json -entry syntax_test5 -code apps/dsl2-samples/out/syntax_test5.cc
+nodejs  compiler/src2/vtcompiler.js apps/dsl2-samples/sys.vt apps/dsl2-samples/syntax_test5.vt -config apps/dsl2-samples/compile-config.json -entry syntax_test5 -code apps/dsl2-samples/out/syntax_test5.cc
